48316e916eb4f62e88043166492a556fb64c904e
MrNoFunsadface <152047068+MrNoFunsadface@users.noreply.github.com>
2025-06-04 20:57:54 +0700

    Commitment omg omg veri cool

---
+++ Assets/Scripts/Menu Scripts/Controllers/InventoryController.cs
@@ -0,0 +1,152 @@
+using Scripts.Models;
+using Scripts.Menu;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Scripts.Controllers
+{
+    public class InventoryController : MonoBehaviour
+    {
+        [SerializeField]
+        private MenuPage inventoryUI;
+
+        [SerializeField]
+        private InventorySO inventoryData;
+
+        [SerializeField]
+        private OptionBar optionBar;
+
+        [SerializeField]
+        private ItemDatabaseSO itemDatabase;
+
+        public List<InventoryItem> initialItems = new List<InventoryItem>();
+
+        private void Start()
+        {
+            PrepareUI();
+            PrepareInventoryData();
+            PrepareOptionBar();
+            InitializeMockData();
+        }
+
+        private void PrepareOptionBar()
+        {
+            optionBar.OnInventorySelected += HandleInventoryToggle;
+        }
+
+        private void HandleInventoryToggle(int obj)
+        {
+            inventoryUI.Show();
+        }
+
+        private void InitializeMockData()
+        {
+            inventoryData.AddItem(itemDatabase.items[0], 1);
+            inventoryData.AddItem(itemDatabase.items[1], 1);
+            inventoryData.AddItem(itemDatabase.items[2], 1);
+            inventoryData.AddItem(itemDatabase.items[3], 1);
+        }
+
+        private void PrepareInventoryData()
+        {
+            inventoryData.Initialize();
+            inventoryData.OnInventoryChanged += UpdateInventoryUI;
+            foreach (InventoryItem item in initialItems)
+            {
+                if (item.isEmpty) continue;
+                inventoryData.AddItem(item);
+            }
+        }
+
+        private void UpdateInventoryUI(Dictionary<int, InventoryItem> inventoryState)
+        {
+            inventoryUI.ResetAllItems();
+            foreach (var item in inventoryState)
+            {
+                inventoryUI.UpdateData(item.Key,
+                    item.Value.itemData.ItemSprite,
+                    item.Value.quantity);
+            }
+        }
+
+        private void PrepareUI()
+        {
+            inventoryUI.InitializeInventoryUI(inventoryData.InventorySize);
+            inventoryUI.OnDescriptionRequested += HandleDescription;
+            inventoryUI.OnSwapItems += HandleSwapItems;
+            inventoryUI.OnStartDragging += HandleStartDragging;
+            inventoryUI.OnItemActionRequested += HandleItemAction;
+        }
+
+        private void HandleItemAction(int itemIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleStartDragging(int itemIndex)
+        {
+            inventoryUI.ResetSelection();
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.isEmpty) return;
+
+            inventoryUI.CreateDraggedItem(
+                inventoryItem.itemData.ItemSprite,
+                inventoryItem.quantity);
+        }
+
+        private void HandleSwapItems(int itemIndex1, int itemIndex2)
+        {
+            inventoryData.SwapItems(itemIndex1, itemIndex2);
+        }
+
+        private void HandleDescription(int itemIndex)
+        {
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.isEmpty)
+            {
+                inventoryUI.ResetSelection();
+                return;
+            }
+            inventoryUI.UpdateDescription(itemIndex,
+                inventoryItem.itemData.ItemSprite,
+                inventoryItem.itemData.AttackStats,
+                inventoryItem.itemData.DefenseStats,
+                inventoryItem.itemData.ItemName,
+                inventoryItem.itemData.ItemDescription,
+                inventoryItem.itemData.Quirk,
+                inventoryItem.itemData.QuirkDescription);
+        }
+
+        public void Update()
+        {
+            if (Keyboard.current.iKey.wasPressedThisFrame)
+            {
+                Debug.Log("I key pressed, toggling inventory UI.");
+                if (inventoryUI.gameObject.activeSelf)
+                {
+                    inventoryUI.Hide();
+                    optionBar.Hide();
+                }
+                else
+                {
+                    optionBar.Show();
+                    inventoryUI.Show();
+                    foreach (var item in inventoryData.GetCurrentInventoryState())
+                    {
+                        if (item.Value.itemData == null || item.Value.itemData.ItemSprite == null)
+                        {
+                            Debug.LogWarning($"Item at key {item.Key} is not properly initialized.");
+                            continue; // Skip this item
+                        }
+
+                        inventoryUI.UpdateData(item.Key, item.Value.itemData.ItemSprite, item.Value.quantity);
+                    }
+
+                }
+            }
+        }
+    }
+}
