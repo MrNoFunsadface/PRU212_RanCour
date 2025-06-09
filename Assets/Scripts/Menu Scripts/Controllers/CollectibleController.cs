13af81a75ac7ef1ef642d507cee4fd45583af8fb
Nguyễn Khang Bình <binhnkse182311@fpt.edu.vn>
2025-06-09 14:42:56 +0700

    added collectible refab

---
+++ Assets/Scripts/Menu Scripts/Controllers/CollectibleController.cs
@@ -0,0 +1,52 @@
+using Scripts.Models;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CollectibleController : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+
+    [SerializeField]
+    private InventorySO inventoryData;
+
+    [SerializeField]
+    private ItemDatabaseSO itemDatabase;
+
+    private bool playerInRange = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            Debug.Log("player steps in " + itemName + "'s hitbox");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            Debug.Log("player steps out " + itemName + "'s hitbox");
+        }
+    }
+
+    void Update()
+    {
+        if (playerInRange && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            var item = itemDatabase.GetItemByName(itemName);
+            if (item == null)
+            {
+                Debug.LogWarning($"Item with name {itemName} not found in database.");
+                return;
+            }
+            inventoryData.AddItem(item, 1);
+            gameObject.SetActive(false);
+            Debug.Log($"Added {itemName} to inventory.");
+        }
+    }
+}
